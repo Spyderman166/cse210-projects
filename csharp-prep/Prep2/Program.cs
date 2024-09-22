41e6a20881a5b8c6bb0a8129972bea0dcab18242
spyderman166 <ryderb516@gmail.com>
2024-09-21 17:03:19 -0700

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gpString = Console.ReadLine();
+        int gradePer = int.Parse(gpString);
+        int remainder = 0;
+        string sign = "";
+        string letter = "";
+        
+        if (gradePer >= 90) {
+            letter = "A";
+        }
+        else if (gradePer >= 80) {
+            letter = "B";
+        }
+        else if (gradePer >= 70) {
+            letter = "C";
+        }
+        else if (gradePer >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+        
+        int quotient = Math.DivRem(gradePer, 10, out remainder);
+        
+        if (remainder >= 7 && letter != "A" && letter != "F") {
+            sign = "+";
+        }
+        else if (remainder <= 3 && letter != "F") {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if (gradePer >= 70) {
+            Console.WriteLine("You passed!");
+        }
+        else {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
