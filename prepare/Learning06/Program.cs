b5fa1963027de2ae5ca6343c919728b25dad82ba
spyderman166 <ryderb516@gmail.com>
2024-10-22 16:17:26 -0700

    Learning06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Blue", 6));
+
+        shapes.Add(new Rectangle("Green", 3, 5));
+
+        shapes.Add(new Circle("Yellow",4));
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor() + "," + shape.GetArea());
+        }
+        
     }
 }
